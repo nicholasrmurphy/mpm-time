209ceb1a4d8b92b0547fed5f7717154e4a1a4741
Nicholas Murphy <nicholasmurphy@Nicholass-MacBook-Pro.local>
2019-03-15 10:36:30 -0500

    updated UI. must fix intermittent undefined buildings

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const {ensureAuthenticated} = require('../config/auth');
+const Entry = require('../models/Entry');
+const Building = require('../models/Building');
+
 
 //Welcome page
 router.get('/', (req,res) => res.render('welcome'));
 
 //dashboard
 router.get('/dashboard',ensureAuthenticated, (req,res) =>{
-  res.render('dashboard', {
-    firstName: req.user.firstName,
-    lastName: req.user.lastName,
+  var buildingsData;
+  Building.find({}, function(err, data) {
+    buildingsData = data;
   });
+  Entry.find({}, function(err, data) {
+      res.render('dashboard',{
+        result:data,
+        firstName: req.user.firstName,
+        lastName: req.user.lastName,
+        buildings: buildingsData
+      });
+    });
 });
 
 module.exports = router;
